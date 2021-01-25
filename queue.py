8def8cae43f2c1b44b010b834371ec6c911383c5
xlincw0w <khaled.khazem@yahoo.com>
2021-01-25 19:47:52 +0100

    dequeue return elem

--- queue.py
+++ queue.py
@@ -7,7 +7,10 @@ class Queue:
 
     def dequeue(self, elem):
         if (self.isEmpty()):
+            elem = self.elements[0]
             self.elements = self.elements[1:]
+
+            return elem
 
     def isEmpty(self):
         if (len(self.elements == 0)):
